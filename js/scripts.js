41df19c73c681ed12064d6bb7c8027949d0289f9
Catherine Karra and Nick Lyman <catherine@>
2016-04-07 11:57:47 -0700

    Working on constructor and function for determining a win.

--- js/scripts.js
+++ js/scripts.js
@@ -18,15 +18,26 @@ Game.prototype.switchPlayer = function () { //uses the if else function to switc
   }
 }
 
-// function Board (number) { //unsure if this is needed or not???//
-//   var board = [];
-//   for (var x = 0; x <= number; x++);
-//     board.push([]);
-//     for (var y = 0; y <= number; y++);
-//       board.push([]);
-//
-//       return board;
-// }
+Game.prototype.win = function () {
+  var win = ["square00", "square01", "square02"];
+  if (this.currentPlayer.spaces.contains[0,1,2] === win) {
+    alert("Yeah, you won!")
+  } else {
+    alert("You haven't won yet!")
+  }
+}
+
+
+function Board (number) { //unsure if this is needed or not???//
+  var number = 2;
+  var board = [];
+  for (var x = 0; x <= number; x++);
+    board.push([], [], []);
+    for (var y = 0; y <= number; y++);
+      board.push([], [], []);
+  return board;
+  console.log(board);
+}
 
 //user interface logic//
 $(document).ready(function() {
@@ -35,16 +46,21 @@ $(document).ready(function() {
 
   var newGame = new Game(playerOne, playerTwo); //instantiate a new instance of the Game object
 
+
   $("#gameBoard td").one("click", function(event) {
     var spaceId = $(this).attr('id'); //using the table ID and each table cell ID to allow for click functions//
     newGame.currentPlayer.spaces.push(spaceId); //pushes the table cell value of clicked cell to current players array//
 
     if (newGame.currentPlayer === playerOne) {
       $("#" + spaceId).text(playerOne.mark); //prints out X or O to game board//
-    } else if (newGame.currentPlayer === playerTwo) {
+    } else {
       $("#" + spaceId).text(playerTwo.mark);
     }
     newGame.switchPlayer(); //switches player to alternate turns//
+    newGame.win();
+
+
+    // if (playerOne.mark)
   });
 
   $("form.chooseXO").submit(function(event) {
